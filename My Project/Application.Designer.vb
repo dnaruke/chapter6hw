e4d01b7d4a9d974d5782c12e6e41bec380d6890e
Daiki Naruke <daikinaruke@student.midlandstech.edu>
2019-10-25 06:11:16 -0400

    Final Commit

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.chapter6hw.Form1
+            Me.MainForm = Global.chapter6hw.frmCornMaze
         End Sub
     End Class
 End Namespace
